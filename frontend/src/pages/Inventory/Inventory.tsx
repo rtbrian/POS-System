936b6c35109d64829bd3f06037ddba8d5140d475
rtbrian <gacherubrian93@gmail.com>
2026-01-13 02:59:29 +0300

    build debug

--- frontend/src/pages/Inventory/Inventory.tsx
+++ frontend/src/pages/Inventory/Inventory.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getProducts, createProduct, deleteProduct type Product } from '../../services/inventoryservice';
+import { getProducts, createProduct, deleteProduct, type Product } from '../../services/inventoryservice';
 import { useNavigate } from 'react-router-dom';
 import { 
   ArrowLeft, 
